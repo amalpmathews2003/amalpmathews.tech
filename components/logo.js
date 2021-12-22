a8b0d580e633089f12ead1dafd3047dca90442e1
amalpmathews2003 <amalpmathews2003@gmail.com>
2021-12-22 18:42:52 +0530

    navbar added

---
+++ components/logo.js
@@ -0,0 +1,41 @@
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const logoBox = styled.span`
+font-weight: bold;
+font-size: 18px;
+display: inline-flex;
+align-items: center;
+height: 30px;
+line-height: 20px;
+padding: 10px;
+img{
+  transition: 200ms ease;
+}
+&:hover img{
+  transform: rotate(20deg);
+}
+`
+
+export default function Logo() {
+  const logoImage = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  return (
+    <Link href="/">
+      <a>
+        <logoBox>
+          <Image src={logoImage} width={20} height={20} alt='logo'></Image>
+          <Text
+            color={useColorModeValue('grey.800', 'whiteAlpha.900')}
+            fontFamily={'M PLUS Rouded 1c'}
+            fontWeight={"bold"}
+            ml={3}
+          >
+            Amal P Mathews
+          </Text>
+        </logoBox>
+      </a>
+    </Link>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react'
+import Layout from '../components/layout/main'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Website({ Component, pageProps,router }) {
+
+  return (
+    <ChakraProvider>
+      <Layout router={router} >
+        <Component {...pageProps} key={router.router}/>
+      </Layout>
+    </ChakraProvider>
+  )
+
 }
 
-export default MyApp
+export default Website
